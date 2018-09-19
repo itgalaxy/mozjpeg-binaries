aa68a6ea2233359972ba3c2fe54ba731c458b546
evilebottnawi <sheo13666q@gmail.com>
2018-09-19 14:10:54 +0300

    chore(release): 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,9 @@ All notable changes to this project will be documented in this file.
 
 This project adheres to [Semantic Versioning](http://semver.org).
 
-## HEAD
+## v1.1.0 - 2018-09-18
 
+- feat: pre-build linux binaries
 - fix: sequential check binaries on working.
 
 ## v1.0.2 - 2018-09-18
--- src/bin-wrappers.js
+++ src/bin-wrappers.js
@@ -2,21 +2,47 @@
 
 const path = require("path");
 const BinWrappers = require("bin-wrapper");
+const pkg = require("../package.json");
 
 const dest = path.join(__dirname, "../vendor");
 const isWin = process.platform === "win32";
 
+const url = `https://raw.githubusercontent.com/itgalaxy/mozjpeg-binaries/v${
+  pkg.version
+}/vendor/`;
+
 module.exports = {
-  cjpeg: new BinWrappers().dest(dest).use(isWin ? "cjpeg.exe" : "cjpeg"),
-  djpeg: new BinWrappers().dest(dest).use(isWin ? "djpeg.exe" : "djpeg"),
+  cjpeg: new BinWrappers()
+    .src(`${url}linux/cjpeg`, "linux")
+    .src(`${url}linux/libjpeg.a`, "linux")
+    .src(`${url}linux/libjpeg.la`, "linux")
+    .src(`${url}linux/libturbojpeg.a`, "linux")
+    .src(`${url}linux/libturboj1peg.la`, "linux")
+    .dest(dest)
+    .use(isWin ? "cjpeg.exe" : "cjpeg"),
+
+  djpeg: new BinWrappers()
+    .src(`${url}linux/djpeg`, "linux")
+    .dest(dest)
+    .use(isWin ? "djpeg.exe" : "djpeg"),
+
   jpegtran: new BinWrappers()
+    .src(`${url}linux/jpegtran`, "linux")
     .dest(dest)
     .use(isWin ? "jpegtran.exe" : "jpegtran"),
+
   rdjpgcom: new BinWrappers()
+    .src(`${url}linux/rdjpgcom`, "linux")
     .dest(dest)
     .use(isWin ? "rdjpgcom.exe" : "rdjpgcom"),
-  tjbench: new BinWrappers().dest(dest).use(isWin ? "tjbench.exe" : "tjbench"),
+
+  tjbench: new BinWrappers()
+    .src(`${url}linux/tjbench`, "linux")
+    .dest(dest)
+    .use(isWin ? "tjbench.exe" : "tjbench"),
+
   wrjpgcom: new BinWrappers()
+    .src(`${url}linux/wrjpgcom`, "linux")
     .dest(dest)
     .use(isWin ? "wrjpgcom.exe" : "wrjpgcom")
 };
