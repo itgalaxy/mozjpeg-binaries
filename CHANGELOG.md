ca584fbe265ed0b648cc690a08efefedaf89723e
evilebottnawi <sheo13666q@gmail.com>
2018-09-19 13:31:23 +0300

    fix: sequential check binaries on working

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 
 This project adheres to [Semantic Versioning](http://semver.org).
 
+## HEAD
+
+- fix: sequential check binaries on working.
+
 ## v1.0.2 - 2018-09-18
 
 - fix: binary installation
--- src/install.js
+++ src/install.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const os = require("os");
 const binBuild = require("bin-build");
+const PQueue = require("p-queue");
 const binWrappers = require("./bin-wrapper");
 
 const buildCommands = () => {
@@ -25,63 +26,70 @@ const buildCommands = () => {
   );
 };
 
-Promise.all(
-  Object.keys(binWrappers).map(program => {
-    const binWrapper = binWrappers[program];
+const queue = new PQueue({ concurrency: 1 });
 
-    return (
-      Promise.resolve()
-        // Workaround https://github.com/kevva/bin-wrapper/issues/67
-        // Need use bin.run(['--version']) after resolve
-        .then(() => binWrapper.findExisting())
-        // eslint-disable-next-line no-sync
-        .then(() => fs.chmodSync(binWrapper.path(), "755"))
-        .then(() => {
-          const commandWihtoutCheck = [
-            "rdjpgcom",
-            "wrjpgcom",
-            "tjbench"
-          ].includes(program);
-          const checkCommand = commandWihtoutCheck ? ["-help"] : ["-version"];
+Object.keys(binWrappers).forEach(program => {
+  const binWrapper = binWrappers[program];
 
-          return binWrapper
-            .runCheck(checkCommand)
-            .then(binCheck => {
+  queue.add(() =>
+    Promise.resolve()
+      // Workaround https://github.com/kevva/bin-wrapper/issues/67
+      // Need use bin.run(['--version']) after resolve
+      .then(() => binWrapper.findExisting())
+      // eslint-disable-next-line no-sync
+      .then(() => fs.chmodSync(binWrapper.path(), "755"))
+      .then(() => {
+        const commandWihtoutCheck = [
+          "rdjpgcom",
+          "wrjpgcom",
+          "tjbench"
+        ].includes(program);
+        const checkCommand = commandWihtoutCheck ? ["-help"] : ["-version"];
+
+        return binWrapper
+          .runCheck(checkCommand)
+          .then(binCheck => {
+            console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
+
+            return binCheck;
+          })
+          .catch(error => {
+            const { message } = error;
+
+            if (commandWihtoutCheck && /usage/iu.test(message)) {
               console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
 
-              return binCheck;
-            })
-            .catch(error => {
-              const { message } = error;
+              return;
+            }
 
-              if (commandWihtoutCheck && /usage/iu.test(message)) {
-                console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
+            throw error;
+          });
+      })
+      .catch(error => {
+        queue.pause();
 
-                return;
-              }
+        console.log(error.message); // eslint-disable-line no-console
+        console.log("pre-build test failed"); // eslint-disable-line no-console
+        console.log("compiling from source"); // eslint-disable-line no-console
 
-              throw error;
-            });
-        })
-    );
-  })
-).catch(error => {
-  console.log(error.message); // eslint-disable-line no-console
-  console.log("pre-build test failed"); // eslint-disable-line no-console
-  console.log("compiling from source"); // eslint-disable-line no-console
+        buildCommands()
+          .then(result => {
+            console.log("mozjpeg binaries built successfully"); // eslint-disable-line no-console
 
-  buildCommands()
-    .then(result => {
-      console.log("built successfully"); // eslint-disable-line no-console
+            return result;
+          })
+          .catch(buildError => {
+            console.log(buildError.stack); // eslint-disable-line no-console
 
-      return result;
-    })
-    .catch(buildError => {
-      console.log(buildError.stack); // eslint-disable-line no-console
+            const exitCode =
+              typeof buildError.code === "number" ? buildError.code : 1;
 
-      const exitCode =
-        typeof buildError.code === "number" ? buildError.code : 1;
+            process.exit(exitCode); // eslint-disable-line no-process-exit
+          });
 
-      process.exit(exitCode); // eslint-disable-line no-process-exit
-    });
+        queue.clear();
+      })
+  );
 });
+
+queue.start();
