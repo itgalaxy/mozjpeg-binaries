2ddcb876e3a99aa446f3acea3af9032d0e0f3309
evilebottnawi <sheo13666q@gmail.com>
2019-07-10 00:46:41 +0300

    chore(release): 2.0.0

--- src/install.js
+++ src/install.js
@@ -7,7 +7,7 @@ const binBuild = require("bin-build");
 const { default: PQueue } = require("p-queue");
 const binWrappers = require("./bin-wrappers");
 
-const buildCommands = () => {
+const buildCommands = async () => {
   const cpus = (os.cpus() || { length: 1 }).length;
 
   let cfgExtras = "";
@@ -26,7 +26,7 @@ const buildCommands = () => {
     `--prefix="${compilationDest}" --bindir="${compilationDest}" --libdir="${compilationDest}"`
   ].join(" ");
 
-  return binBuild.url(
+  await binBuild.url(
     "https://github.com/mozilla/mozjpeg/archive/v3.3.1.tar.gz",
     ["autoreconf -fiv", cfg, `make -j${cpus}`, `make install -j${cpus}`]
   );
@@ -37,76 +37,72 @@ const queue = new PQueue({ concurrency: 1 });
 Object.keys(binWrappers).forEach(program => {
   const binWrapper = binWrappers[program];
 
-  queue.add(() =>
-    Promise.resolve()
-      .then(() => {
-        if (process.env.COMPILATION_REQUIRED) {
-          throw new Error("Compilation required");
-        }
+  queue.add(async () => {
+    try {
+      if (process.env.COMPILATION_REQUIRED) {
+        throw new Error("Compilation required");
+      }
 
-        // eslint-disable-next-line promise/no-return-wrap
-        return Promise.resolve();
-      })
       // Workaround https://github.com/kevva/bin-wrapper/issues/67
       // Need use bin.run(['--version']) after resolve
-      .then(() => binWrapper.findExisting())
+      await binWrapper.findExisting();
+
       // eslint-disable-next-line no-sync
-      .then(() => fs.chmodSync(binWrapper.path(), "755"))
-      .then(() => {
-        const commandWihtoutCheck = [
-          "rdjpgcom",
-          "tjbench",
-          "wrjpgcom"
-        ].includes(program);
-        const checkCommand = commandWihtoutCheck ? ["-help"] : ["-version"];
+      fs.chmodSync(binWrapper.path(), "755");
 
-        return binWrapper
-          .runCheck(checkCommand)
-          .then(binCheck => {
-            console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
+      const commandWihtoutCheck = ["rdjpgcom", "tjbench", "wrjpgcom"].includes(
+        program
+      );
+      const checkCommand = commandWihtoutCheck ? ["-help"] : ["-version"];
 
-            return binCheck;
-          })
-          .catch(error => {
-            const { message } = error;
+      try {
+        await binWrapper.runCheck(checkCommand);
+      } catch (error) {
+        const { message } = error;
 
-            if (commandWihtoutCheck && /usage/i.test(message)) {
-              console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
+        if (commandWihtoutCheck && /usage/i.test(message)) {
+          console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
 
-              return;
-            }
-
-            throw error;
-          });
-      })
-      .catch(error => {
-        queue.pause();
-
-        if (!process.env.COMPILATION_REQUIRED) {
-          console.log(error.message); // eslint-disable-line no-console
-          console.log("pre-build test failed"); // eslint-disable-line no-console
+          return binWrapper;
         }
 
-        console.log("compiling from source"); // eslint-disable-line no-console
+        throw error;
+      }
 
-        buildCommands()
-          .then(result => {
-            console.log("mozjpeg binaries built successfully"); // eslint-disable-line no-console
+      console.log(`${program} pre-build test passed successfully`); // eslint-disable-line no-console
 
-            return result;
-          })
-          .catch(buildError => {
-            console.log(buildError.stack); // eslint-disable-line no-console
+      return binWrapper;
+    } catch (error) {
+      queue.pause();
 
-            const exitCode =
-              typeof buildError.code === "number" ? buildError.code : 1;
+      if (!process.env.COMPILATION_REQUIRED) {
+        console.log(error.message); // eslint-disable-line no-console
+        console.log("pre-build test failed"); // eslint-disable-line no-console
+      }
 
-            process.exit(exitCode);
-          });
+      console.log("compiling from source"); // eslint-disable-line no-console
 
-        queue.clear();
-      })
-  );
+      // eslint-disable-next-line init-declarations
+      let result;
+
+      try {
+        result = await buildCommands();
+      } catch (buildError) {
+        console.log(buildError.stack); // eslint-disable-line no-console
+
+        const exitCode =
+          typeof buildError.code === "number" ? buildError.code : 1;
+
+        process.exit(exitCode);
+      }
+
+      console.log("mozjpeg binaries built successfully"); // eslint-disable-line no-console
+
+      queue.clear();
+
+      return result;
+    }
+  });
 });
 
 queue.start();
