a88f167dd351bab9416d57163d099b193fadd37c
evilebottnawi <sheo13666q@gmail.com>
2019-07-09 22:34:53 +0300

    ci: test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,13 +3,23 @@
 const path = require("path");
 const paths = require("../src");
 
+const isWin = process.platform === "win32";
+
 describe("paths", () => {
   it("should be exported", () => {
-    expect(path.basename(paths.cjpeg)).toBe("cjpeg");
-    expect(path.basename(paths.djpeg)).toBe("djpeg");
-    expect(path.basename(paths.jpegtran)).toBe("jpegtran");
-    expect(path.basename(paths.rdjpgcom)).toBe("rdjpgcom");
-    expect(path.basename(paths.tjbench)).toBe("tjbench");
-    expect(path.basename(paths.wrjpgcom)).toBe("wrjpgcom");
+    expect(path.basename(paths.cjpeg)).toBe(isWin ? "cjpeg.exe" : "cjpeg");
+    expect(path.basename(paths.djpeg)).toBe(isWin ? "djpeg.exe" : "djpeg");
+    expect(path.basename(paths.jpegtran)).toBe(
+      isWin ? "jpegtran.exe" : "jpegtran"
+    );
+    expect(path.basename(paths.rdjpgcom)).toBe(
+      isWin ? "rdjpgcom.exe" : "rdjpgcom"
+    );
+    expect(path.basename(paths.tjbench)).toBe(
+      isWin ? "tjbench.exe" : "tjbench"
+    );
+    expect(path.basename(paths.wrjpgcom)).toBe(
+      isWin ? "wrjpgcom.exe" : "wrjpgcom"
+    );
   });
 });
--- src/bin-wrappers.js
+++ src/bin-wrappers.js
@@ -4,7 +4,10 @@ const path = require("path");
 const BinWrappers = require("bin-wrapper");
 const pkg = require("../package.json");
 
-const dest = path.join(__dirname, "../vendor");
+const dest =
+  typeof process.env.COMPILATION_DEST !== "undefined"
+    ? process.env.COMPILATION_DEST
+    : path.join(__dirname, "../vendor");
 const isWin = process.platform === "win32";
 
 const url = `https://raw.githubusercontent.com/itgalaxy/mozjpeg-binaries/v${pkg.version}/vendor/`;
@@ -13,46 +16,42 @@ module.exports = {
   cjpeg: new BinWrappers()
     .src(`${url}linux/cjpeg`, "linux")
     .src(`${url}macos/cjpeg`, "darwin")
-    .src(`${url}windows/x64/cjpeg.exe`, "win32", "x64")
-    .src(`${url}windows/x86/cjpeg.exe`, "win32", "x86")
+    .src(`${url}windows/cjpeg.exe`, "win32")
     .dest(dest)
     .use(isWin ? "cjpeg.exe" : "cjpeg"),
 
   djpeg: new BinWrappers()
     .src(`${url}linux/djpeg`, "linux")
     .src(`${url}macos/djpeg`, "darwin")
-    .src(`${url}windows/x64/djpeg.exe`, "win32", "x64")
-    .src(`${url}windows/x86/djpeg.exe`, "win32", "x86")
+    .src(`${url}windows/djpeg.exe`, "win32")
     .dest(dest)
     .use(isWin ? "djpeg.exe" : "djpeg"),
 
   jpegtran: new BinWrappers()
     .src(`${url}linux/jpegtran`, "linux")
     .src(`${url}macos/jpegtran`, "darwin")
-    .src(`${url}windows/x64/jpegtran.exe`, "win32", "x64")
-    .src(`${url}windows/x86/jpegtran.exe`, "win32", "x86")
+    .src(`${url}windows/jpegtran.exe`, "win32")
     .dest(dest)
     .use(isWin ? "jpegtran.exe" : "jpegtran"),
 
   rdjpgcom: new BinWrappers()
     .src(`${url}linux/rdjpgcom`, "linux")
     .src(`${url}macos/rdjpgcom`, "darwin")
-    .src(`${url}windows/x64/rdjpgcom.exe`, "win32", "x64")
-    .src(`${url}windows/x86/rdjpgcom.exe`, "win32", "x86")
+    .src(`${url}windows/rdjpgcom.exe`, "win32")
     .dest(dest)
     .use(isWin ? "rdjpgcom.exe" : "rdjpgcom"),
 
   tjbench: new BinWrappers()
     .src(`${url}linux/tjbench`, "linux")
     .src(`${url}macos/tjbench`, "darwin")
+    .src(`${url}windows/tjbench.exe`, "win32")
     .dest(dest)
     .use(isWin ? "tjbench.exe" : "tjbench"),
 
   wrjpgcom: new BinWrappers()
     .src(`${url}linux/wrjpgcom`, "linux")
     .src(`${url}macos/wrjpgcom`, "darwin")
-    .src(`${url}windows/x64/wrjpgcom.exe`, "win32", "x64")
-    .src(`${url}windows/x86/wrjpgcom.exe`, "win32", "x86")
+    .src(`${url}windows/wrjpgcom.exe`, "win32")
     .dest(dest)
     .use(isWin ? "wrjpgcom.exe" : "wrjpgcom")
 };
