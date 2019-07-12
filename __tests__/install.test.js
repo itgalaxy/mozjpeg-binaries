c7ecf499b338b327bd0647903fd3b2fb3a5e6490
Evilebot Tnawi <evilebottnawi@users.noreply.github.com>
2019-07-12 16:13:24 +0300

    feat: env variables on install phase (#30)

--- __tests__/install.test.js
+++ __tests__/install.test.js
@@ -9,37 +9,28 @@ const binaries = require("../src");
 // eslint-disable-next-line unicorn/prefer-exponentiation-operator
 const infinityTimeout = Math.pow(2, 31) - 1;
 
-describe("command", () => {
+describe("install", () => {
   it(
-    "should rebuild the mozjpeg binaries",
-    () =>
-      execa("node", [path.join(__dirname, "../src/install.js")]).then(
-        result => {
-          expect(result.exitCode).toBe(0);
+    "should build the mozjpeg binaries and minify",
+    async () => {
+      const result = await execa.node(
+        path.join(__dirname, "../src/install.js")
+      );
 
-          return result;
-        }
-      ),
+      expect(result.exitCode).toBe(0);
+
+      const tmp = tempy.directory();
+      const src = path.join(__dirname, "fixtures/test.jpg");
+      const dest = path.join(tmp, "test.jpg");
+      const args = ["-outfile", dest, src];
+      const binResult = await execa(binaries.cjpeg, args);
+
+      expect(binResult.exitCode).toBe(0);
+
+      const compareResult = await compareSize(src, dest);
+
+      expect(compareResult[dest]).toBeLessThan(compareResult[src]);
+    },
     infinityTimeout
   );
-
-  it("cjpeg should minify a JPG", () => {
-    const tmp = tempy.directory();
-    const src = path.join(__dirname, "fixtures/test.jpg");
-    const dest = path.join(tmp, "test.jpg");
-    const args = ["-outfile", dest, src];
-
-    return execa(binaries.cjpeg, args)
-      .then(result => {
-        expect(result.exitCode).toBe(0);
-
-        return result;
-      })
-      .then(() => compareSize(src, dest))
-      .then(result => {
-        expect(result[dest]).toBeLessThan(result[src]);
-
-        return result;
-      });
-  });
 });
